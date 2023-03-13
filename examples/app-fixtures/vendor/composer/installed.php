261d1eea628d9fafe253e732a86ef793ceed9bd7
Laurent Laville <laurent.laville@gmail.com>
2023-03-13 07:59:55 +0000

    add programmatically example

---
+++ examples/app-fixtures/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'root/app-fixtures',
+        'pretty_version' => '3.x-dev',
+        'version' => '3.9999999.9999999.9999999-dev',
+        'reference' => '2e728d08899946a0e4e5ab2fc5507236e12f71a7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'psr/log' => array(
+            'pretty_version' => '3.0.0',
+            'version' => '3.0.0.0',
+            'reference' => 'fe5ea303b0887d5caefd3d431c3e61ad47037001',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+        'root/app-fixtures' => array(
+            'pretty_version' => '3.x-dev',
+            'version' => '3.9999999.9999999.9999999-dev',
+            'reference' => '2e728d08899946a0e4e5ab2fc5507236e12f71a7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ examples/phar-manifest.php
@@ -0,0 +1,68 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the BoxManifest package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once  dirname(__DIR__) . '/vendor/autoload.php';
+
+use Bartlett\BoxManifest\Composer\ManifestFactory;
+
+use KevinGH\Box\Box;
+use KevinGH\Box\Console\Php\PhpSettingsHandler;
+use function KevinGH\Box\get_box_version;
+
+use Psr\Log\AbstractLogger;
+
+/**
+ * Script that may build manifest in different formats :
+ * - SBOM JSON or XML format following specification 1.1, 1.2, 1.3, or 1.4
+ * - Simple key-value pairs TEXT format without decoration
+ * - key-value pairs TEXT format with ansi decoration
+ *
+ * @author Laurent Laville
+ * @since Release 3.0.0
+ */
+
+$logger = new class extends AbstractLogger {
+    public function log($level, string|Stringable $message, array $context = []): void
+    {
+        echo sprintf('[%s] %s%s', $level, $message, PHP_EOL);
+    }
+};
+
+(new PhpSettingsHandler($logger))->check();
+
+$configLoader = new KevinGH\Box\Configuration\ConfigurationLoader();
+
+$config = $configLoader->loadFile(__DIR__ . '/app-fixtures/app-fixtures-box.json');
+$box = Box::create($config->getTmpOutputPath());
+
+$factory = new ManifestFactory($config, $box, get_box_version());
+
+try {
+    // 1.
+    $format = 'xml'; // Allowed values are: xml, json
+    $specVersion = '1.4'; // Allowed values are: 1.1, 1.2, 1.3, 1.4
+    $result = $factory->toSbom($format, $specVersion);
+
+    // 2.
+    //$result = $factory->toText();
+
+    // 3.
+    //$result = $factory->toHighlight();
+} catch (Throwable $e) {
+    echo $e->getMessage(), PHP_EOL;
+} finally {
+    $logger->log(
+        'info',
+        'Using composer.json : ' . $config->getComposerJson()
+    );
+    $logger->log(
+        'info',
+        'Using composer.lock : ' . $config->getComposerLock()
+    );
+    echo $result, PHP_EOL;
+}
