597fb5dba9bcded4efca7039bcd6930678a8c08f
Laurent Laville <laurent.laville@gmail.com>
2025-12-08 06:35:35 +0000

    fixed SC3014 shellcheck error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-[ "$APP_DEBUG" == 'true' ] && set -x
+[ "$APP_DEBUG" = 'true' ] && set -x
 set -e
 
 composer_global_home="/home/$(id -u -n)/.composer"
 
-if [ "$APP_DEBUG" == 'true' ]
+if [ "$APP_DEBUG" = 'true' ]
 then
     echo "> You will act as user: $(id -u -n)"
     echo "> Path to Composer home dir: ${composer_global_home}"
